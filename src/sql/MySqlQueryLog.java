fe5581eee16330f6c5209328d94218f366bf30b4
raging-loon <cxmacolley@gmail.com>
2021-07-27 15:14:39 -0400

    new file:   src/sql/MySqlQueryLog.java

---
+++ src/sql/MySqlQueryLog.java
@@ -0,0 +1,16 @@
+package src.sql;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySqlQueryLog {
+  public String filename;
+  public String sqlVersion;
+  public List<String> QTime = new ArrayList<String>();
+  public List<Integer> QId =  new ArrayList<Integer>();
+  public List<String> QCommand = new ArrayList<String>();
+  public List<String> QArgs = new ArrayList<String>();
+  public MySqlQueryLog(String filename){
+    this.filename = filename;
+  }
+}
