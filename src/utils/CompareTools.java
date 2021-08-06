8949ffeb1b12730b69ab7cbf25a004991b8a3ff0
raging-loon <cxmacolley@gmail.com>
2021-08-06 13:20:53 -0400

    modified:   test2.java

--- src/utils/CompareTools.java
+++ src/utils/CompareTools.java
@@ -14,19 +14,23 @@ public class CompareTools {
 
   public static HashMap<String, Integer> compareLogs(LogObject x, LogObject y){
     HashMap<String, Integer> ipAddrFound = new HashMap<>();
+    
     HashMap<String,Integer> ipCountsLog1 = getIpCounts((LogFormat)x);
     HashMap<String,Integer> ipCountsLog2 = getIpCounts((LogFormat)y);
     
     if(ipCountsLog1 == null || ipCountsLog2 == null){
       return null;
     }
-
     for(String addr: ipCountsLog1.keySet()){
-
+      if(ipAddrFound.get(addr) == null && ipCountsLog2.get(addr) != null){
+         ipAddrFound.put(addr,ipCountsLog1.get(addr) + ipCountsLog2.get(addr));
+      } 
     }
 
     return ipAddrFound; 
   }
+
+
   public static HashMap<String,Integer> getIpCounts(LogFormat x){
     HashMap<String,Integer> ipCounts = new HashMap<>();
     if(x.getIpAddresses() == null){
--- test2.java
+++ test2.java
@@ -2,7 +2,6 @@
 import java.util.HashMap;
 
 import src.http.Apache2Log;
-import src.misc.Vsftpd;
 import src.utils.CompareTools;
 // import src.*;
 // import src.http.*;
@@ -10,10 +9,14 @@ import src.utils.CompareTools;
 public class test2 {
   // VSFTPD TEST
   public static void main(String[] args) {
-    Apache2Log log = new Apache2Log("/var/log/access.log");
+    Apache2Log log = new Apache2Log("tests/weblog1.log");
     log.Parser();
-
-    HashMap<String,Integer> x =  CompareTools.getIpCounts(log);
+    Apache2Log log1 = new Apache2Log("tests/access.log");
+    log1.Parser();
+    HashMap<String,Integer> x =  CompareTools.compareLogs(log,log1);
+    if(x == null){
+      System.exit(-1);
+    }
     for(String i: x.keySet()){
       System.out.println(i + ": " + x.get(i));
     }
